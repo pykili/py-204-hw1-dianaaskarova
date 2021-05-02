28d007132d30407d1a7ac6c69b36346e5ffd2dd8
dianaaskarova <78154378+dianaaskarova@users.noreply.github.com>
2021-05-02 16:11:04 +0300

    Update solution_11.py

--- task_11/solution_11.py
+++ task_11/solution_11.py
@@ -1,1 +1,12 @@
-# your code here
+left = input()
+right = input()
+ln = len(left)
+rn = len(right)
+i = 0
+is_there = False
+while i <= rn - ln:
+    if right[i:i+ln] == left:
+        is_there = True
+        break
+    i += 1
+print(is_there)
