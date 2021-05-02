20182edf2e4176b835e452cb91147c86a2b76cc0
dianaaskarova <78154378+dianaaskarova@users.noreply.github.com>
2021-05-02 15:37:07 +0300

    Update solution_7.py

--- task_7/solution_7.py
+++ task_7/solution_7.py
@@ -1,8 +1,8 @@
 s = input()
 is_palindrom = True
 
-for i in range(len(s)//2):
-    if s[i]!=s[-i-1]:
-        is_palindrom=False
+for i in range(len(s) // 2):
+    if s[i] != s[-i - 1]:
+        is_palindrom = False
         break
 print(is_palindrom)
