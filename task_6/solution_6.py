df5863b75cc908177aed582693f40c3baee3f823
dianaaskarova <78154378+dianaaskarova@users.noreply.github.com>
2021-05-02 15:36:01 +0300

    Update solution_6.py

--- task_6/solution_6.py
+++ task_6/solution_6.py
@@ -2,7 +2,7 @@ n = int(input())
 if n != 0:
     print('_')
 for i in range(n):
-    if i == n-1:
-        print((2*i + 1)*' '+'|')
+    if i == n - 1:
+        print((2 * i + 1) * ' ' + '|')
     else:
-        print((2*i + 1)*' '+'|_')
+        print((2 * i + 1) * ' ' + '|_')
